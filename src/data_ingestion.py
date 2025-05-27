f69a8c6984ce05bde90c82b344dcba323d7c7164
PRANAV_REDDY <reddysaipranav6@gmail.com>
2025-05-27 13:02:46 +0530

    Solved-Environment-Issue

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,13 +1,17 @@
-import pandas as pd 
+import pandas as pd
 import os
+
 def data_ingestion():
-    base_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    # Get the base directory where the script is located
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     train_path = os.path.join(base_dir, "data", "train.csv")
-    df_train = pd.read_csv(train_path)
-
     test_path = os.path.join(base_dir, "data", "test.csv")
+
+    df_train = pd.read_csv(train_path)
     df_test = pd.read_csv(test_path)
-    return df_train,df_test
-if __name__=='__main__':
+
+    return df_train, df_test
+
+if __name__ == '__main__':
     data_ingestion()
